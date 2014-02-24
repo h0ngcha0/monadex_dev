457f9c3b3a8ee9da98e0df7844cf3a04c199cf3e
Hongchao Liu <lbs.lhc@gmail.com>
2014-02-24 06:38:24 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MonadexDev::Application.config.secret_key_base = '7a80a64f0d09705039afd367756363830c462000e5ae4f8aef2c711df34c435fd6f5b893c5e0a3545a22ad4e0e96d59b02a32a54fd60ae1ffc600c5f321ec034'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MonadexDev::Application.config.session_store :cookie_store, key: '_monadex_dev_session'
